2a8c562025678b34cf9c67a3b08532ef31de37d5
abeiseke <aika.b.2005@gmail.com>
2025-02-21 10:11:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const MONGO_URI = "mongodb+srv://Aika:080116bs@online-store.kl09t.mongodb.net/?retryWrites=true&w=majority&appName=online-store";
+require('dotenv').config();
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("MongoDB connection error:", err));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
